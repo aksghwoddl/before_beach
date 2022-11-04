557f69fac854911576283a916d08f23c97a20014
aksghwoddl <aksghwoddl@naver.com>
2022-11-04 18:54:22 +0900

    second commit

---
+++ BeachCongetionPJ/app/src/main/java/com/lee/beachcongetion/BeachCongestionModel.kt
@@ -0,0 +1,8 @@
+package com.lee.beachcongetion
+data class BeachCongestionModel(
+    val etlDT : String ,
+    val seqID : Int ,
+    val poiNm : String ,
+    val uniqPop : String ,
+    val congestion : String
+)
--- BeachCongetionPJ/app/src/main/java/com/lee/beachcongetion/MainActivity.kt
+++ BeachCongetionPJ/app/src/main/java/com/lee/beachcongetion/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lee.beachcongetion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lee.beachcongetion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }
